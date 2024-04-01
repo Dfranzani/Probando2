750ebc01f73c06db93be79a9186ff910020b6433
Dfranzani <dfranzani@gmail.com>
2024-04-01 17:58:41 -0300

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig } from 'astro/config';
-//import { adapter as staticAdapter } from '@astrojs/adapter-static';
 
 export default defineConfig({
   site: 'https://dfranzani.github.io',
-  //output: "dist",
-  //adapter: staticAdapter({
-  //  pages: 'dist'
-  //})
+  output: "dist",
 });
