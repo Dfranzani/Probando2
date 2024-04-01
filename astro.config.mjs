70c268358e3db6b33819a142b6902658ac85588d
Dfranzani <dfranzani@gmail.com>
2024-04-01 17:06:33 -0300

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  //site: 'https://dfranzani.github.io/astro-cv-minimalista/',
+  site: 'https://dfranzani.github.io',
+  base: '/astro-cv-minimalista'
   //output: 'static',
   //outDir: './docs',
-  output: "server",
-  adapter: vercel()
+  //output: "server",
+  //adapter: vercel()
 });
