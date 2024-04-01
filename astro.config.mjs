1638e8e6b16766802f5133cb40b1ac9c01bbc441
Dfranzani <dfranzani@gmail.com>
2024-04-01 00:12:47 -0300

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dfranzani.github.io',
-//  output: "server",
-//  adapter: vercel()
+  output: "server",
+  adapter: vercel()
 });
