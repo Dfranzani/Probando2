ad1d3faac7d647d384ef7d331a1cf00f90cb1f58
Dfranzani <dfranzani@gmail.com>
2024-04-02 21:55:46 -0300

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://dfranzani.github.io',
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/probando2/' : '/',
 });
